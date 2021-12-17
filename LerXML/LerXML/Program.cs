43ca857d9daed647aac1e1611eba6a55aad919d1
Marcio de Andrade <marcio@correnteeletrica.com.br>
2021-12-17 19:27:59 -0400

    Comandos

---
+++ LerXML/LerXML/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml;
+
+namespace LerXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlTextReader reader = new XmlTextReader(@"C:/Labs/XmlEstudos/livros.xml");
+            Console.WriteLine("Informações Gerais");
+            Console.WriteLine("==================");
+            Console.WriteLine(reader.Name);
+            Console.WriteLine(reader.BaseURI);
+            Console.WriteLine(reader.LocalName);
+
+        }
+    }
+}
---
+++ Solution1/CriaXML/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml;
+
+namespace CriaXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cria novo arquivo 
+            XmlTextWriter writer = new XmlTextWriter("C:\\Labs\\ArquivoXML.xml", null);
+        }
+    }
+}
