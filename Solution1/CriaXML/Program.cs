67e7f7e784a1c45f6719803c3516a16ea35d880c
Marcio de Andrade <marcio@correnteeletrica.com.br>
2021-12-17 19:41:18 -0400

    Update Program.cs

--- Solution1/CriaXML/Program.cs
+++ Solution1/CriaXML/Program.cs
@@ -9,6 +9,28 @@ namespace CriaXML
         {
             //Cria novo arquivo 
             XmlTextWriter writer = new XmlTextWriter("C:\\Labs\\ArquivoXML.xml", null);
+            //abre cocumento
+            writer.WriteStartDocument();
+            //escreve comentários
+            writer.WriteComment("Este programa usa XmlTextWriter");
+            writer.WriteComment("Desenvolvido por: Marcio");
+            writer.WriteComment("===============================");
+
+            //escreve o primeiro elemento
+            writer.WriteStartElement("root");
+            writer.WriteStartElement("r", "Record", "urn: record");
+
+            //escreve o próximo elemento
+            writer.WriteStartElement("Nome", "");
+            writer.WriteString("Marcio");
+            writer.WriteEndElement();
+
+            //escreve mais um elemento
+            writer.WriteStartElement("Sobrenome", "");
+            writer.WriteString("Andrade");
+            writer.WriteEndElement();
+
+
         }
     }
 }
